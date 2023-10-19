9b13e604f45cfa14f64f9ba9c0bba8c1e4ee3336
Indergrafic <120105130+indergrafic@users.noreply.github.com>
2023-10-19 20:34:35 +0200

    Update img_to_text.py
    Creación del botón "Iniciar", y el cuadro de texto.

--- img_to_text.py
+++ img_to_text.py
@@ -1,37 +1,52 @@
 import tkinter as tk
 from tkinter import ttk
-
+# ------------------ Ventana Raiz ---------------------
 root = tk.Tk()
-
 root.title("Convert-Text")
 icono = tk.PhotoImage(file="text-img.png")
 root.iconphoto(True, icono)
-root.geometry("500x300")
-root.resizable(width=False, height=True)
+root.geometry("500x500")
+root.resizable(width=True, height=True)
 root.config(background='#292d3e')
 
 
-
-
+# ----------------- Elementos Interiores ----------------
 label_info = ttk.Label(root, text='Extraiga texto de una imagen con Convert-Text ', 
                       background='#292d3e', 
                       foreground='#7FFFD4',
                       font= ('Arial', 15))
-label_info.grid(row=0, columnspan=2, padx=30, pady=40)
+label_info.grid(row=0, column=0, columnspan=2, padx=20, pady=40)
 
 
 button_ruta = tk.Button(root, 
-                         text='Buscar Ruta',
+                         text='Buscar Imagen',
+                         font=('Arial', 8),
                          bg='#00008B',
                          foreground='white')
-button_ruta.grid(row=1, column=0,padx=20, sticky='ew')
+button_ruta.grid(row=1, column=0, padx=20, ipadx=2, ipady=2, pady=5)
 
-entry_ruta = ttk.Entry(root, 
-                       background='#0000CD', 
+entry_ruta = ttk.Label(root, 
+                       background=('#3b4159'),
                        width=40, 
                        font=('Arial', 11))
-entry_ruta.grid(row=1, column=1, sticky='e')
+entry_ruta.grid(row=1, column=1, padx=5, pady=5)
 
+button_ruta = tk.Button(root, 
+                         text='Iniciar',
+                         font=('Arial', 12),
+                         bg='#00008B',
+                         foreground='white')
+button_ruta.grid(row=2, column=0, columnspan=2, pady=20, ipadx=40, ipady=10)
+
+text_captura = tk.Text(root,
+                       bg='blue',
+                       font=('Arial', 12),
+                       foreground='white',
+                       border=10,
+                       width=50,
+                       height=30,
+                       autoseparators=True)
+text_captura.grid(row=3, column=0, columnspan=2, padx=10, pady=20)
 
 
 
