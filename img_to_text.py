830c4d458a7106493766380a7dcf10322080996f
Indergrafic <120105130+indergrafic@users.noreply.github.com>
2023-10-23 12:59:04 +0200

    Update img_to_text.py

--- img_to_text.py
+++ img_to_text.py
@@ -1,6 +1,14 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+
+
+#------------------ Funciones ----------------------
+def busqueda_ruta():
+    mensaje = ('Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.')
+    
+
+
 # ------------------ Ventana Raiz ---------------------
 root = tk.Tk()
 root.title("Convert-Text")
@@ -32,10 +40,11 @@ button_ruta = tk.Button(buttom_frame,
                          relief='flat')
 button_ruta.grid(row=1, column=0, padx=20, ipadx=2, ipady=2, pady=5)
 
-entry_ruta = ttk.Label(buttom_frame, 
+mensajetexto = StringVar()
+entry_ruta = tk.Label(buttom_frame, 
                        background=('#3b4159'),
                        width=40, 
-                       font=('Arial', 11))
+                       font=('Arial', 11),)
 entry_ruta.grid(row=1, column=1, padx=5, pady=5, sticky='w')
 
 button_ruta = tk.Button(buttom_frame, 
@@ -43,29 +52,26 @@ button_ruta = tk.Button(buttom_frame,
                          font=('Helvetica', 12, "bold"),
                          bg='#00008B',
                          foreground='white',
-                         relief='flat')
+                         relief='flat',
+                         command=busqueda_ruta)
 button_ruta.grid(row=2, column=0, columnspan=2, pady=20, ipadx=40, ipady=10)
 
 scroll_barra = ttk.Scrollbar(frame_text,
                              orient='vertical')
 scroll_barra.pack(fill='y',side='right')
 
-
-text_captura = tk.Text(frame_text,
+mensajetexto = StringVar()
+text_captura = tk.Label(frame_text,
                        bg='blue',
                        font=('Arial', 12),
                        foreground='white',
-                       width=50,
-                       height=10,
-                       wrap='none',
-                       yscrollcommand=scroll_barra.set)
+                       text=mensajetexto)
 text_captura.pack(fill='both', padx=5, pady=5)
 borde_frama_text = tk.LabelFrame(frame_text, text='Texto Escaneado', font=('Arial', 10), background='white')
 borde_frama_text.config(bd=2)
 borde_frama_text.pack(padx=2, pady=2)
 
 
-scroll_barra.config(command=text_captura.yview, style='')
 
 
 # ------------------ Barra de Menus ----------------------------
