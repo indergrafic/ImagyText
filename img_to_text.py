f5fee2adc22eb8d491e4b28179f1cafabcae9abd
Indergrafic <120105130+indergrafic@users.noreply.github.com>
2023-10-24 18:20:10 +0200

    Funcionalidades Añadidas
    Ventanas de información para solventar errores en la búsqueda o cuando se inicie la conversión a texto.
    Creación de la barra de Menús y sus funcionalidades.

--- img_to_text.py
+++ img_to_text.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter import messagebox
 from PIL import Image
 import pytesseract
 
@@ -8,26 +9,64 @@ import pytesseract
 root = Tk()
 
 root.title('Convert-Text')
-root.resizable(True,True)
+root.resizable(False, False)
 root.iconbitmap('text-Img.ico')
+# ---------------- Ventanas secundarias -------------------
+def error_busqueda():
+    respuesta = messagebox.askretrycancel("Error en la Busqueda", 'Archivo o Ruta especificada\n no es correcta.')
+    if respuesta == True:
+        abrir_archivo()
 
 # ---------------- Instruciones para la conversión -------------------
 def abrir_archivo():
+    ''' Función que nos permite hacer la busqueda del archivo que escanearemos su texto.'''
     global archivo
-    archivo = filedialog.askopenfilenames(title='abrir')
+    try:
+        archivo = filedialog.askopenfilenames(title="Buscar Archivo", 
+                                            initialdir="C:/",
+                                            filetypes=(("Archivos de imagen",''), ('JPG','*.jpg'), ('PNG','*.png'), ('BMP','*.bmp')))
+    except: error_busqueda()
+
+    '''Atraves del config, modificamos el texto de la Etiqueta, para mostrar el path del archivo.'''
     label_ruta.config(text=archivo[0])
-    print(archivo[0])
 
 def convert_text():
-    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
-    ruta_imagen= archivo[0]
-    imagen_abierta = Image.open(ruta_imagen, mode='r')
-    texto = pytesseract.image_to_string(imagen_abierta)
-    label_text_scan.config(text=texto)
+    ''' Función con la que abrimos el programa que realiza la tarea de conversión.
+    Ser realiza una apertura del archivo, el escaneo del texto y su guardado, y lo enviamos
+     a la etiqueta que lo imprime en pantalla. '''
+    try:    
+        pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+        ruta_imagen= archivo[0]
+        imagen_abierta = Image.open(ruta_imagen, mode='r')
+        texto = pytesseract.image_to_string(imagen_abierta)
+        label_text_scan.config(text=texto)
+    except: error_busqueda()
     
+# --------------- Barra de Menus y Funcionalidades ------------------------
+def salir_Aplicacion():
+    valor = messagebox.askquestion(title='Salir', message='¿Desea salir de la Aplicación?')
+    if valor == 'yes':
+        root.destroy()
+
+def info_Aplicacion():
+    messagebox.showinfo(title='Información', message='Conversor de texto en formato imagen a formato texto\n Vesion 1.0 (Octubre 2023)')
+
+barra_Menu = Menu(root)
+root.config(menu=barra_Menu, width=300, height=200)
+
+opcion_Archivo = Menu(barra_Menu, tearoff=0)
+opcion_Archivo.add_command(label='Salir', command=salir_Aplicacion)
+
+opcion_Info = Menu(barra_Menu, tearoff=0)
+opcion_Info.add_command(label='Ayuda', command=info_Aplicacion)
+
+barra_Menu.add_cascade(label='Archivo', menu=opcion_Archivo)
+barra_Menu.add_cascade(label='Info', menu=opcion_Info)
 
 # ---------------- Ventanas Secundarias -------------------
+''' El primer frame el donde colocamos el texto de presentacion y botones para
+buscar la ruta e iniciar el escaneo. El segundo lo conpone botones de guardado y
+traduccion, y el labelFrame que muestra el texto.'''
 frame_botones = Frame()
 frame_botones.pack(fill='both', expand='True')
 frame_botones.config(bg='#333051', width=500, height=200, cursor='hand2')
@@ -57,10 +96,9 @@ boton_buscar = Button(frame_botones,
 boton_buscar.grid(row=1, column=0, padx=20, pady=20, ipadx=3, ipady=3)
 
 label_ruta = Label(frame_botones, text='..\indica la ruta del archivo.',
-                   font=('Times', 12, 'bold'), 
+                   font=('Arial', 8, 'bold'), 
                    fg='#71729C',
-                   bg='#333051',
-                   width=40)
+                   bg='#333051',anchor='center')
 label_ruta.grid(row=1, column=1, padx=5, pady=20)
 
 boton_iniciar = Button(frame_botones, 
@@ -86,7 +124,7 @@ label_text_scan = Label(cuadro_texto,
                         justify='left',
                         fg='white',
                         bg='#333051',
-                        width=40)
+                        width=60, anchor='center')
 label_text_scan.grid(row=0, column=0, sticky='ew')
 
 
